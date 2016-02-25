CyanogenMod/android_device_oneplus_oneplus2

Set proper CPU variants
Change-Id: Ibe4d944f54b8f9b7555b0ba87fbcd72c7bae6aab
Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

BoardConfig.mk
@@ -43,13 +43,13 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT := cortex-a7
 
 TARGET_CPU_CORTEX_A53 := true
 

Hardslog/android_device_oneplus_oneplus2test

BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 TARGET_CPU_CORTEX_A53 := true
 